1f67d9b7459ea9dc483b83a709c70de7c72880f1
michalmacenka <my@michalmacenka.cz>
2022-08-29 23:04:00 +0200

    chore: cors, helmet

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import Fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import fastifyHelmet from '@fastify/helmet';
 
 import schoolsRoutes from './modules/schools/schools.route';
 
@@ -7,6 +9,12 @@ const port = 3000;
 
 const main = async () => {
   app.register(schoolsRoutes, { prefix: 'schools-list' });
+
+  app.register(fastifyCors, {
+    credentials: true,
+    origin: true,
+  });
+  app.register(fastifyHelmet, { global: true });
 
   try {
     await app.listen({ port });
